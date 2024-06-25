5e743262dc38626d3f1e7c27b95194d906cec838
o3etornam <03etornam@gmail.com>
2024-06-25 07:52:16 +0000

    added course components

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import Sidebar, { SidebarItem } from '../components/Sidebar'
+import React from "react";
+import Sidebar, { SidebarItem } from "../components/Sidebar";
 import {
-    NotebookPen,
-    UserCircle,
-    LayoutDashboard,
-    Settings,
-    LifeBuoy
-  } from 'lucide-react'
-import { UserProvider } from '../components/UserProvider'
+  NotebookPen,
+  UserCircle,
+  LayoutDashboard,
+  Settings,
+  LifeBuoy,
+} from "lucide-react";
+import { UserProvider } from "../components/UserProvider";
+import NavBar from "../components/NavBar";
 
 function Courses() {
   return (
-    <div>
-        <UserProvider>
-        <Sidebar>
-        <SidebarItem icon={<LifeBuoy size={20} />} text="Help"/>
-        <SidebarItem icon={<Settings size={20} />} text="Settings" to={'/settings'}/>
-        </Sidebar>
-        </UserProvider>
+    <div className="flex">
+      <Sidebar>
+        <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+        <SidebarItem
+          icon={<Settings size={20} />}
+          text="Settings"
+          to={"/settings"}
+        />
+      </Sidebar>
+      <NavBar />
     </div>
-  )
+  );
 }
 
-export default Courses
+export default Courses;
